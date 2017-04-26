a72fa8b0c92b29aa1ce1dae65fbcef86506d2ce2
vdnguyen58 <vdnguyen58@gmail.com>
2017-04-26 00:48:28 -0700

    Make base class

--- base.py
+++ base.py
@@ -5,64 +5,70 @@ from Adafruit_PWM_Servo_Driver import PWM
 
 # Initialise the PWM device using the default address
 # bmp = PWM(0x40, debug=True)
-pwm = PWM(0x40, debug=True)
 
-servoMin = 175
-servoMax = 625
+class RobotBase():
+  pwm = PWM(0x40, debug=True)
 
-def setServoPulse(channel, pulse):
-  pulseLength = 1000000                   # 1,000,000 us per second
-  pulseLength /= 60                       # 60 Hz
-  print "%d us per period" % pulseLength
-  pulseLength /= 4096                     # 12 bits of resolution
-  print "%d us per bit" % pulseLength
-  pulse *= 1000
-  pulse /= pulseLength
-  pwm.setPWM(channel, 0, pulse)
+  servoMin = 175
+  servoMax = 625
 
-def sendPWM(mtNum, en, in1, in2):
+  def setServoPulse(self, channel, pulse):
+    pulseLength = 1000000                   # 1,000,000 us per second
+    pulseLength /= 60                       # 60 Hz
+    print "%d us per period" % pulseLength
+    pulseLength /= 4096                     # 12 bits of resolution
+    print "%d us per bit" % pulseLength
+    pulse *= 1000
+    pulse /= pulseLength
+    self.pwm.setPWM(channel, 0, pulse)
+
+  def sendPWM(self, mtNum, en, in1, in2):
     en *= 4095
     in1 *= 4095
     in2 *= 4095
-    pwm.setPWMFreq(50)
+    self.pwm.setPWMFreq(50)
     if (mtNum == 0):
-        pwm.setPWM(2, 0, en)
-        pwm.setPWM(4, 0, in1)
-        pwm.setPWM(5, 0, in2)
+        self.pwm.setPWM(2, 0, en)
+        self.pwm.setPWM(4, 0, in1)
+        self.pwm.setPWM(5, 0, in2)
     else:
-        pwm.setPWM(3, 0, en)
-        pwm.setPWM(6, 0, in1)
-        pwm.setPWM(7, 0, in2)
+        self.pwm.setPWM(3, 0, en)
+        self.pwm.setPWM(6, 0, in1)
+        self.pwm.setPWM(7, 0, in2)
 
-def stop():
+  def stop(self):
     print "turn on"
     # left wheel
-    sendPWM(0, 1, 0, 0)
+    self.sendPWM(0, 1, 0, 0)
     # right wheel
-    sendPWM(1, 1, 0, 0)
+    self.sendPWM(1, 1, 0, 0)
     # i.e.: turn left(0) wheel on(1) and set the state to stop(0), (0)
 
-def turnoff():
+  def turnoff(self):
     print "turn off"
-    sendPWM(0, 0, 0, 0)
-    sendPWM(1, 0, 0, 0)
+    self.sendPWM(0, 0, 0, 0)
+    self.sendPWM(1, 0, 0, 0)
 
-def forward():
+  def forward(self):
     print "forward"
-    sendPWM(0, 1, 0, 1)
-    sendPWM(1, 1, 1 ,0)
+    self.sendPWM(0, 1, 0, 1)
+    self.sendPWM(1, 1, 1 ,0)
     # turn left wheel (0) on (1) and let it run forward (0, 1)
     # turn right wheel (1) on (1) and let it run backward (1, 0)
 
-def backward():
+  def backward(self):
     print "backward"
-    sendPWM(0, 1, 1, 0)
-    sendPWM(1, 1, 0, 1)
+    self.sendPWM(0, 1, 1, 0)
+    self.sendPWM(1, 1, 0, 1)
 
-#forward()
-#time.sleep(10)
-#stop()
-#time.sleep(10)
-#backward()
-#time.sleep(10)
-#stop()
+  def test(self):
+    self.forward()
+    time.sleep(10)
+    self.stop()
+    time.sleep(10)
+    self.backward()
+    time.sleep(10)
+    self.stop()
+
+darwin=RobotBase()
+darwin.test()
